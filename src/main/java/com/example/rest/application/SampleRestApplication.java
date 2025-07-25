93c8e287863d5bfd79de03c613e5cdf47f5f008d
anuradha balasubramanian <anuradha.balasubramanian@capgemini.com>
2025-07-25 14:50:08 +0530

    initial destination files

---
+++ src/main/java/com/example/rest/application/SampleRestApplication.java
@@ -0,0 +1,19 @@
+package com.example.rest.application;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+import java.util.Set;
+import java.util.HashSet;
+
+import com.example.rest.resource.SampleRestResource;
+
+@ApplicationPath("/sample")
+public class SampleRestApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(SampleRestResource.class);
+        return classes;
+    }
+}
